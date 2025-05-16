4ec8791bbbb935f0e001bdf0126f51c63c03759a
Eduardo Pessoa <eduardo.pessoa@estudante.ifmt.edu.br>
2025-05-16 10:43:36 -0300

    Projeto Inicializado

---
+++ Aula 2504/src/aplicacao/Programa.java
@@ -0,0 +1,22 @@
+package aplicacao;
+
+import entidades.ServidorPublico;
+
+public class Programa
+{
+    public static void main(String[] args) {
+
+        ServidorPublico isabela = new ServidorPublico();
+        isabela.setNome("Isabela");
+        isabela.setCargo("Auditor");
+        isabela.setOrgao("ANVISA");
+        isabela.setLotacao("Brasília");
+        isabela.setEmail("isabela@gmail.com");
+
+        System.out.println("Servidora: "+isabela.getNome());
+
+        ServidorPublico heila = new ServidorPublico();
+        ServidorPublico maria = new ServidorPublico();
+        ServidorPublico caio = new ServidorPublico();
+    }
+}
